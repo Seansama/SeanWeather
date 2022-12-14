e579fe2c9be820068e46c4e176d26dfac517239d
Shaun <seansama@pop-os.localdomain>
2022-12-14 12:40:32 +0300

    Added variables for input and errors

---
+++ Authentication/Authy.js
@@ -0,0 +1,5 @@
+let email = document.forms['form']['email'];
+let password = document.forms['form']['password'];
+
+let email_error = document.getElementById('email_error');
+let pass_error = document.getElementById('pass_error');
