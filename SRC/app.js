35ab1138e17bcb64312e72dc0ba281c856b2f5bc
Shaun <seansama@pop-os.localdomain>
2022-12-14 21:23:38 +0300

    Made api call with fetch

--- SRC/app.js
+++ SRC/app.js
@@ -1,3 +1,21 @@
 document.addEventListener(`DOMContentLoaded`, () => {
+    let weather = {
+        apiKey: '6b021e2a2b26c9a4141168c1b149d7fa';
+        fetchWeather: function (city) {
+            fetch(
+                "https://api.openweathermap.org/data/2.5/weather?q=" +
+                city +
+                "&units=metric&appid=" +
+                this.apiKey
+            )
+                .then((response) => {
+                    if (!response.ok) {
+                        alert("No weather found.");
+                        throw new Error("No weather found.");
+                    }
+                    return response.json();
+                })
+                .then((data) => this.displayWeather(data));
+        },
 
 })
